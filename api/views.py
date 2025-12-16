d474b61a4ee6c247f55ae54f86b39caac0cc9a23
Edward <eric199703@hotmail.com>
2025-12-16 08:43:00 -0300

    test: add tests with pytest-django

--- api/views.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-
-# Create your views here.
-=======
-from django.shortcuts import render
-
-# Create your views here.
->>>>>>> c3e5ec581c243edc5370c8b26fd150284cd93280
